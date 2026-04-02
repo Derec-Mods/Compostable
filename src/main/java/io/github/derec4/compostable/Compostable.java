55a0f054be8971a1b61fe222ca10bac69a4ebd66
Derek <derex1987@gmail.com>
2026-04-01 20:26:31 -0500

    feat: init

---
+++ src/main/java/io/github/derec4/compostable/Compostable.java
@@ -0,0 +1,17 @@
+package io.github.derec4.compostable;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Compostable extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
